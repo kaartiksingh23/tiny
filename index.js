f535868979483913295144a7393b63b0c28d99a2
kaartiksingh23 <kaartiksingh23@gmail.com>
2019-07-02 17:39:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
-};
+module.exports = {
+  replace: function (string,toBeReplaced,toBeReplacedWith) {
+                            if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+                            if(toBeReplaced === undefined || toBeReplaced === null){
+                              throw new TypeError("Argument 2 cannot be undefined or null")
+                            }
+                            if(toBeReplacedWith === undefined || toBeReplacedWith === undefined ){
+                              throw new TypeError("Argument 3 cannot be undefined or null")
+                            }
+                            let replace = toBeReplaced;
+                            let regex = new RegExp(replace,"g"); // "g" indicates to continue search even if the string is found.
+                            return string.replace(regex,toBeReplacedWith)
+                          }
+  
+  
+}
