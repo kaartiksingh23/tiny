0590c900c6dfcfd4139605e1888a0dc257824991
kaartiksingh23 <kaartiksingh23@gmail.com>
2019-07-08 13:06:59 +0530

    added Concat function

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 module.exports = {
   replace: function (string,toBeReplaced,toBeReplacedWith) {
-                            if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-                            if(toBeReplaced === undefined || toBeReplaced === null){
-                              throw new TypeError("Argument 2 cannot be undefined or null")
-                            }
-                            if(toBeReplacedWith === undefined || toBeReplacedWith === undefined ){
-                              throw new TypeError("Argument 3 cannot be undefined or null")
-                            }
-                            let replace = toBeReplaced;
-                            let regex = new RegExp(replace,"g"); // "g" indicates to continue search even if the string is found.
-                            return string.replace(regex,toBeReplacedWith)
-                          }
+    
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+
+    if(toBeReplaced === undefined || toBeReplaced === null){
+      throw new TypeError("Argument 2 cannot be undefined or null")
+    }
+
+    if(toBeReplacedWith === undefined || toBeReplacedWith === undefined ){
+      throw new TypeError("Argument 3 cannot be undefined or null")
+    }
+
+    let replace = toBeReplaced;
+    let regex = new RegExp(replace,"g"); // "g" indicates to continue search even if the string is found.
+    return string.replace(regex,toBeReplacedWith)
   
+  },
+  concat: function (listOfStrings) {
+    
+    if (!Array.isArray(listOfStrings)) throw new TypeError("Tiny wants an array of strings!");
+    let concattedString='';
+
+    listOfStrings.forEach(string => {
+      concattedString=concattedString + string;
+    });
+
+    return concattedString;
   
+  }
 }
